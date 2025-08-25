67eab25ba144eee3a42f58e527fe17b6b5f53d27
Aayush Gour <138443486+itsaayush27@users.noreply.github.com>
2025-08-25 13:12:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+functions daysOfAYear(year) {
+	  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366; // Leap year
+  } else {
+    return 365; // Non-leap year
+  }
+}
+console.log(2022);
+	
+}
